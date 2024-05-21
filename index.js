23013e6f27be25259396d9b252ce03e5267d4891
Willian Mamede <williansilvamamede@gmail.com>
2024-05-21 00:19:49 -0300

    Adicionado desafio e resolução do desafio.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+let ranked = function winRate(wins, loses){
+    let calculateWinRate = wins - loses;
+    let elo;
+    let stringResult = `O Herói tem de saldo de ${calculateWinRate} está no nível de ${elo}`;
+
+    if(calculateWinRate <= 10){
+        elo = "Ferro";
+    } else if (calculateWinRate < 20) {
+        elo = "Bronze";
+    } else if (calculateWinRate < 50) {
+        elo = "Prata";
+    } else if (calculateWinRate < 80) {
+        elo = "Ouro";
+    } else if (calculateWinRate < 90) {
+        elo = "Diamante";
+    } else if (calculateWinRate < 100) {
+        elo = "Lendário";
+    } else {
+        elo = "Imortal";
+    }
+    return stringResult;
+}
+
+let wins = 50;
+let loses = 10;
+
+console.log(ranked(wins, loses));
